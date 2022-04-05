8866daf2779a8035807bdbfea44d32b981d0a4d1
andrii-venher <vengerandr@gmail.com>
2022-04-06 00:41:53 +0200

    Add charging adapter status check

--- bar-functions/dwm_battery.sh
+++ bar-functions/dwm_battery.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-# A dwm_bar function to read the battery level and status
+# A dwm_bar function to read the battery level and BAT_STATUS
 # Joe Standring <git@joestandring.com>
 # GNU GPLv3
 
 dwm_battery () {
     # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
     BATTERY="BAT0"
-    CHARGE=$(cat /sys/class/power_supply/${BATTERY}/capacity)
-    STATUS=$(cat /sys/class/power_supply/${BATTERY}/status)
-
+    ADAPTER="ADP0"
+    BAT_CHARGE=$(cat /sys/class/power_supply/${BATTERY}/capacity)
+    BAT_STATUS=$(cat /sys/class/power_supply/${BATTERY}/status)
+    ADP_STATUS=$(cat /sys/class/power_supply/${ADAPTER}/online)
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        if [ "$STATUS" = "Charging" ] || [ "$STATUS" = "Full" ]; then
+        if [ "$BAT_STATUS" = "Charging" ] || [ "$BAT_STATUS" = "Full" ] || [ "$ADP_STATUS" = "1" ]; then
             printf "🔌 "
         else
             printf "🔋 "
         fi
-	printf "%s%%" "$CHARGE"
+	printf "%s%%" "$BAT_CHARGE"
     else
-        printf "BAT %s%% %s" "$CHARGE" "$STATUS"
+        printf "BAT %s%% %s" "$BAT_
+CHARGE" "$BAT_STATUS"
     fi
     printf "%s\n" "$SEP2"
 }
